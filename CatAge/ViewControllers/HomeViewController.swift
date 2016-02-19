b587c1e72e4b4abab126b0145dfa99480d872ab8
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-02-19 16:05:23 -0200

    Added some code to turn the ui functional.
    Added code to calculate the cat's age from human age of a cat.

--- CatAge/ViewControllers/HomeViewController.swift
+++ CatAge/ViewControllers/HomeViewController.swift
@@ -11,16 +11,48 @@ import UIKit
 /** The view controller for home view at **Main** storyboard. */
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var catAgeTextField: UITextField!
+    @IBOutlet weak var resultTextField: UILabel!
+    
+    // MARK: UIViewController methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpLayout()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Helper methods
+    
+    func setUpLayout() {
+        resultTextField.text = String()
     }
     
+    func calculateCatAgeFromHumanAge(age: Int) -> Int {
+        let validAge: Int = age - 1
+        
+        return 20 + (validAge * 4)
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func calculateCatAgeAction() {
+        guard catAgeTextField.text?.characters.count > 0 else {
+            resultTextField.text = "Type a valid age"
+            resultTextField.textColor = UIColor.redColor()
+            
+            return
+        }
+        
+        if let ageString:String = catAgeTextField.text {
+            let age:Int = Int(ageString)!
+            
+            let catAge: Int = calculateCatAgeFromHumanAge(age)
+            
+            resultTextField.text = "For your cat its age is: \(catAge) years old"
+            resultTextField.textColor = UIColor.lightGrayColor()
+        }
+    }
     
 }
 
