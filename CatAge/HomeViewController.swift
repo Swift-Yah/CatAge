6d70b9947ded6bf9d8e578ff6c856fc7807319c9
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-02-19 14:55:37 -0200

    Added a initial project for Swift.

--- CatAge/HomeViewController.swift
+++ CatAge/HomeViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+/** The view controller for home view at **Main** storyboard. */
+class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
